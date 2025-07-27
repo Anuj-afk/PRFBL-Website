1dcffe1bc67d25e2b5d0a9ab4ba2aa626738c2e3
anuj-afk <anujg6263@gmail.com>
2025-07-27 16:11:07 +0530

    Remove admin verification for fetching pages in CMS routes

--- Server/src/routes/cmsRoutes.js
+++ Server/src/routes/cmsRoutes.js
@@ -11,7 +11,7 @@ import { verifyAdmin } from "../middlewares/verify.js";
 const router = express.Router();
 
 router.post("/pages", verifyAdmin, createPage);
-router.get("/pages/:slug", verifyAdmin, getPage);
+router.get("/pages/:slug", getPage);
 router.post("/pages/:slug/sections", verifyAdmin, addSectionToPage);
 router.put("/sections/:id", verifyAdmin, updateSection);
 router.delete("/sections/:id", verifyAdmin, deleteSection);
